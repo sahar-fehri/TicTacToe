bb40205276fefd4700fcc16971aefd8a0270fe28
sahar <fehrisahar14@gmail.com>
2018-04-05 23:36:08 +0100

    " "

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -13,7 +13,7 @@ contract Tic{
 
 
     uint id =0;
-    uint availS = 9;
+    // uint availS = 9;
 
     Game[] public games;
 
@@ -37,24 +37,24 @@ contract Tic{
 
 
 
-    function makeMove( address _player, uint8 _xy) public returns (bool) { // will return true if one player won and game is over
-        WhoMadeTheMove(_player);
-        ShowTurn(_player);
-        require(availS!=0);
+    function makeMove( uint8 _xy) public returns (bool) { // will return true if one player won and game is over
+        WhoMadeTheMove(msg.sender);
+        ShowTurn(msg.sender);
+
         Game memory g = games[id];
-        require(msg.sender == _player);
-        require(g.turn == _player);
+
+        require(g.turn == msg.sender);
         require(g.board[_xy] == 0);
         require(g.hasWon != true);
-        games[id].board[_xy] = _player;
-        games[id].turn = _player == g.player1 ? g.player2 : g.player1;
+        games[id].board[_xy] = msg.sender;
+        games[id].turn = msg.sender == g.player1 ? g.player2 : g.player1;
 
         ShowTurn(games[id].turn);
 
 
-        if(checkWin(g, _player) == true){
-            FoundWinner(_player);
-            games[id].winner = _player;
+        if(checkWin(g, msg.sender) == true){
+            FoundWinner(msg.sender);
+            games[id].winner = msg.sender;
             games[id].hasWon = true;
 
             return true;
@@ -63,22 +63,22 @@ contract Tic{
         return false;
     }
 
-    function setWinner(Game _g, address _winner) private pure returns (bool) {
+    function setWinner(Game _g, address _winner) private returns (bool) {
         _g.winner = _winner;
         _g.hasWon = true;
         return true;
     }
 
 
-    function availableSPots(Game _game) private returns (uint) {
-        for(uint i=0; i<=9; i++){
-            if(_game.board[i]== 0){
-                availS --;
-            }
-        }
+    // function availableSPots(Game _game) private returns (uint) {
+    //     for(uint i=0; i<=9; i++){
+    //         if(_game.board[i]== 0){
+    //             availS --;
+    //         }
+    //     }
 
-        return availS;
-    }
+    //     return availS;
+    // }
 
     function checkWin(Game _game, address _player) private  returns (bool) {
 
@@ -92,7 +92,7 @@ contract Tic{
             (_game.board[0] == _player && _game.board[4] == _player && _game.board[8] == _player) ||
             (_game.board[2] == _player && _game.board[4] == _player && _game.board[6] == _player)
         ) {
-            EnterWinnerCheck(_player);
+
             setWinner(_game,_player);
             return true;
         } else {
